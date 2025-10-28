62c64b883331a505eaf1ff9977fbeed02b31a3d0
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-29 00:19:20 +0530

    Removing an element from the 1) Beginning 2) End 3) Specific Index of an array

---
+++ Array/CoreConcepts/014_Removing_Element_From_Array.java
@@ -0,0 +1,8 @@
+package Array.CoreConcepts;
+
+class Removing_Element_From_Array {
+    /*
+    In Java, arrays are static or fixed size, i.e it is not possible to increase/decrease the length of the array.
+    It is therefore not possible to add or remove an element to or from the array.
+    */
+}
