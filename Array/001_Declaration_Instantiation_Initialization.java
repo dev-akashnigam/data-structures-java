b124893791d7a1c53cf0b72548b21762ab42ebb8
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 11:04:51 +0530

    Creating an array - Declaration, Instantiation & Initialization

---
+++ Array/001_Declaration_Instantiation_Initialization.java
@@ -0,0 +1,16 @@
+package Array;
+
+class Declaration_Instantiation_Initialization
+{
+    public static void main(String args[]) {
+        
+        // Declaration (declaration only)
+        int[] arr1;
+
+        // Instantiation (declaration and instantiation)
+        int[] arr2 = new int[5]; // allocated memory to an array of 5 integers.
+
+        // Initialization (declaration, instantiation and initialization)
+        int[] arr3 = {10, 20, 30, 40, 50};
+    }
+}
