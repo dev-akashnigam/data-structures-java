9dfb5d33f36a621525b48f971abc00ba68b374d0
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 12:01:03 +0530

    Determining number/count of array elements

---
+++ Array/CoreConcepts/004_Number_Of_Array_Elements.java
@@ -0,0 +1,11 @@
+package Array.CoreConcepts;
+
+class Number_Of_Array_Elements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int numberOfElements = arr.length;
+
+        System.out.printf("Number of elements in the array = %d", numberOfElements);
+    }    
+}
