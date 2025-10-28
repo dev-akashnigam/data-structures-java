174c5bb8b8a75e7564103ff86a9005fd7cab5252
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-29 00:42:10 +0530

    Removing multiple elements from the 1) Beginning 2) End 3) Specific Index of an array

---
+++ Array/CoreConcepts/015_Removing_Multiple_Elements_From_Array.java
@@ -0,0 +1,8 @@
+package Array.CoreConcepts;
+
+class Removing_Multiple_Elements_From_Array {
+    /*
+    In Java, arrays are static or fixed size, i.e it is not possible to increase/decrease the length of the array.
+    It is therefore not possible to add or remove one or more element(s) to or from the array.
+    */    
+}
