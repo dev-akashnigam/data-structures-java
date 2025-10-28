c4b11a505654e586ca20370fe85b67aef1621a17
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-28 23:07:20 +0530

    Iterating over array by both index and element

---
+++ Array/CoreConcepts/011_Iterating_Over_Array_By_Both_Index_And_Element.java
@@ -0,0 +1,5 @@
+package Array.CoreConcepts;
+
+class Iterating_Over_Array_By_Both_Index_And_Element {
+    // There exists no way of iterating over the elements of the array by both index and element in Java.
+}
