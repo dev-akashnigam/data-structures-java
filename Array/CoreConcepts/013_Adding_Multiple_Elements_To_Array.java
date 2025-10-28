78f6134996d6981e8ccf437d6424f9e7a73107e7
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-29 00:05:08 +0530

    Adding multiple elements at the 1) Beginning 2) End 3) Specific Index of an array

---
+++ Array/CoreConcepts/013_Adding_Multiple_Elements_To_Array.java
@@ -0,0 +1,8 @@
+package Array.CoreConcepts;
+
+class Adding_Multiple_Elements_To_Array {
+    /*
+    In Java, arrays are static or fixed size, i.e it is not possible to increase/decrease the length of the array.
+    It is therefore not possible to add or remove one or more element(s) to or from the array.
+    */
+}
