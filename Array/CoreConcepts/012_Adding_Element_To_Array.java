e3b5f603aea34dfe2225ad230ada105d2767bbb8
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-29 12:29:24 +0530

    Reversing an array

--- Array/CoreConcepts/012_Adding_Element_To_Array.java
+++ Array/CoreConcepts/012_Adding_Element_To_Array.java
@@ -2,8 +2,6 @@ package Array.CoreConcepts;
 
 class Adding_Element_To_Array {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
-
         /*
         In Java, arrays are static or fixed size, i.e it is not possible to increase/decrease the length of the array.
         It is therefore not possible to add or remove an element to or from the array.
---
+++ Array/CoreConcepts/017_Reversing_Array.java
@@ -0,0 +1,7 @@
+package Array.CoreConcepts;
+
+class Reversing_Array {
+    public static void main(String[] args) {        
+        // There is no built-in way of reversing an array in Java.
+    } 
+}
