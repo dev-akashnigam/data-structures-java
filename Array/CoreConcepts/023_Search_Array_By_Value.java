c1f4ce80cb0a1b91e6b3e3460b3c09087b281edf
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-01 01:59:59 +0530

    Searching array elements by value

---
+++ Array/CoreConcepts/023_Search_Array_By_Value.java
@@ -0,0 +1,24 @@
+package Array.CoreConcepts;
+
+import java.util.Arrays;
+
+class Search_Array_By_Value {
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 30, 20, 40};
+        System.out.println("Original Array, arr = "+Arrays.toString(arr));
+
+        int searchElement = 20;
+
+        Arrays.sort(arr);
+        int searchResult = Arrays.binarySearch(arr, searchElement);
+
+        if(searchResult>=0) {
+            System.out.printf("Element: %d is present in the array.", searchElement);
+
+            // there exists no built-in method for determining the index (first or last) of the searched element in the array.
+        }
+        else {
+            System.out.printf("Element: %d is NOT present in the array.", searchElement);
+        }
+    }
+}
