4fb19ed4a60279ba3d88aa47cfebf4ad9b6777ff
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-31 16:21:00 +0530

    Array Comprehension = Filtering + Mapping/Transforming

---
+++ Array/CoreConcepts/022_Array_Comprehension_Filtering_Plus_Mapping.java
@@ -0,0 +1,5 @@
+package Array.CoreConcepts;
+
+class Array_Comprehension_Filtering_Plus_Mapping {
+    // No built-in method in Java that supports array comprehension i.e first filtering array elements and then mapping/transforming them.    
+}
