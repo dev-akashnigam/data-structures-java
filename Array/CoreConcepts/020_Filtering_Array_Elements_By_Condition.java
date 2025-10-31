fedf032fc99b7ffb72e038fea80cf328ce3c4b70
Akash Nigam <contact.akashnigam@gmail.com>
2025-10-31 12:57:04 +0530

    Filtering array elements by condition into a new array

---
+++ Array/CoreConcepts/020_Filtering_Array_Elements_By_Condition.java
@@ -0,0 +1,5 @@
+package Array.CoreConcepts;
+
+class Filtering_Array_Elements_By_Condition {
+    // There is no built-in method in Java that supports filtering array elements by condition into a new array.
+}
