ad5e8c5900df8804deed2b6bdd1e8f62fc1925d9
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-04 01:11:27 +0530

    Finding maximum and minimum element in an array

---
+++ Array/CoreConcepts/033_Maximum_And_Minimum_Array_Element.java
@@ -0,0 +1,5 @@
+package Array.CoreConcepts;
+
+class Maximum_And_Minimum_Array_Element {
+    // There is no built-in method in Java that can be used for finding the max and min element of an array. 
+}
