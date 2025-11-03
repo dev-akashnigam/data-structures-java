71739dfa1372f0299d104636b91d94c2f590460f
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-04 00:12:43 +0530

    Determining if a variable is an array

---
+++ Array/CoreConcepts/030_Check_If_Reference_Or_Variable_Points_To_Array.java
@@ -0,0 +1,5 @@
+package Array.CoreConcepts;
+
+class Check_If_Reference_Or_Variable_Points_To_Array {
+    // This is not applicable for Java since java is static i.e each variable is declared with type and that type of variable is determined at compile time.
+}
