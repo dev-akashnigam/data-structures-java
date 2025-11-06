7fb663d470ed82602c1589d7a7cbeda76f72173a
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 13:06:11 +0530

    Finding number of characters in the string

---
+++ String/CoreConcepts/003_NumberOfCharactersInString.java
@@ -0,0 +1,11 @@
+package String.CoreConcepts;
+
+class NumberOfCharactersInString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        int noOfCharacters = str.length();
+
+        System.out.printf("Number of characters in string: %s = %d", str, noOfCharacters);
+    }    
+}
