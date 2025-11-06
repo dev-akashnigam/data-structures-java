d8add6decc9c06b2157750390907477dcf177028
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-06 12:49:39 +0530

    Printing a string on console

---
+++ String/CoreConcepts/002_PrintingString.java
@@ -0,0 +1,11 @@
+package String.CoreConcepts;
+
+class PrintingString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println("Current String printed using println() method = "+str);
+
+        System.out.printf("Current String printed using printf() method = %s", str);
+    }
+}
