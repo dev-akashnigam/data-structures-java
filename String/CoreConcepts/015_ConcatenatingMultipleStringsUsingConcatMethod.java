49b29e334034920a21771341d065e69e6e8fc54c
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-10 01:38:36 +0530

    Concatenating multiple strings using concat method

--- String/CoreConcepts/015_ConcatenatingMultipleStringsUsingConcatMethod.java
+++ String/CoreConcepts/015_ConcatenatingMultipleStringsUsingConcatMethod.java
@@ -1,6 +1,6 @@
 package String.CoreConcepts;
 
-class ConcatenationOfMultipleStringsV1 {
+class ConcatenatingMultipleStringsUsingConcatMethod {
     public static void main(String[] args) {
         String str1 = "Hello";
         System.out.printf("First string = %s\n", str1);
@@ -11,7 +11,7 @@ class ConcatenationOfMultipleStringsV1 {
         String str3 = "World";
         System.out.printf("Third string = %s\n", str3);
 
-        String concatenatedString = str1 + str2 + str3;
+        String concatenatedString = str1.concat(str2).concat(str3);
         System.out.printf("Concatenated string = %s\n", concatenatedString);
-    }    
+    }
 }
