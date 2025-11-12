6d9e29a33d281e67485db80a654d9c3a5c564a56
Akash Nigam <contact.akashnigam@gmail.com>
2025-11-13 01:47:36 +0530

    Determining ASCII value of a given character.

---
+++ String/CoreConcepts/030_CharToAsciiConversion.java
@@ -0,0 +1,11 @@
+package String.CoreConcepts;
+
+class CharToAsciiConversion {
+    public static void main(String[] args) {
+        char ch = 'a';
+
+        int asciiValue = (int)ch;
+
+        System.out.printf("Ascii value of character: %c = %d", ch, asciiValue);
+    }
+}
